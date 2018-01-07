10075c4d07416ee2d30bbecb02acc1000b008d3d
Tomasz <estewez@gmail.com>
2018-01-07 15:45:01 +0100

    Task completed

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,18 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if(input !== null) {
-            process.stdout.write(input);
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit': 
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/ver':
+                console.log(process.versions);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
     }
 });
